3715a983bfce48e38fa8fb4b6da84600875b608a
ruanhongjie690-star <ruanhongjie690@gmail.com>
2025-09-24 02:18:13 +0800

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -27,9 +27,9 @@ import { remarkMermaid } from "./src/plugins/remark-mermaid.js";
 import { remarkReadingTime } from "./src/plugins/remark-reading-time.mjs";
 // https://astro.build/config
 export default defineConfig({
-	site: "https://ruanhongjie690.github.io",
+	site: "https://ruanhongjie690-star.github.io",
 
-	base: "/ruanhongjie690.github.io",
+	base: "/",
 	trailingSlash: "always",
 	integrations: [
 		tailwind({
