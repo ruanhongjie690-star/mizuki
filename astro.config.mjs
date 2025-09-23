884646b86437ce32b46d49794ecdc79626ce3d03
ruanhongjie690-star <ruanhongjie690@gmail.com>
2025-09-24 01:52:57 +0800

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -29,7 +29,7 @@ import { remarkReadingTime } from "./src/plugins/remark-reading-time.mjs";
 export default defineConfig({
 	site: "https://ruanhongjie690.github.io",
 
-	base: "/",
+	base: "ruanhongjie690.github.io",
 	trailingSlash: "always",
 	integrations: [
 		tailwind({
